54a1f987dd7e6ecc5853c14aca364b67d5789955
Christopher <qaetre@gmail.com>
2023-06-13 15:35:20 -0400

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Item({ name, category }) {
+  const [ isInCart, setIsInCart ] = React.useState(false)
+  const cartClass = isInCart ? "in-cart" : ""
+  function addToCartClick () {
+    setIsInCart(!isInCart)
+  }
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCartClick}>{isInCart ? "Remove from Cart" : "Add to Cart" }</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,10 +2,34 @@ import React from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  return (
+  const [ selectedCategory, setSelectedCategory ] = React.useState("All")
+    if (selectedCategory === "Produce") {
+      const newItems = items.filter(item => item.category === "Produce")
+      return (
+        renderShoppingList(newItems)
+      );
+    }
+    if (selectedCategory === "Dairy") {
+      const newItems = items.filter(item => item.category === "Dairy")
+      return(
+        renderShoppingList(newItems)
+      );
+    }
+    if (selectedCategory === "Dessert") {
+      const newItems = items.filter(item => item.category === "Dessert")
+      return(
+        renderShoppingList(newItems)
+      );
+    }
+    else {
+      return (renderShoppingList(items))
+    }
+
+  function renderShoppingList(newItems) {
+    return(
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +37,13 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {newItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
-  );
-}
+    );
+            }
+          }
 
 export default ShoppingList;
